8391297bd530d2bf88902211826a1319ea3542a2
Dynamonic <dylanmckillip@gmail.com>
2019-11-18 23:16:03 -0500

    wrote segments function

--- SliceVis/visual.py
+++ SliceVis/visual.py
@@ -12,5 +12,32 @@ class Visualizer(object):
         self.lines = segment(self.points)
 
     def segment(self, points):
+        edges = []
+        p1 = None
+        p2 = None
+        color = None
         for i in range(len(points)):
             if i != len(points)-1:
+                if points(i) is "ON":
+                    color = 1
+                elif points(i) is "OFF":
+                    color = 2
+                else:
+                    if p1 is None:
+                        xyz = points(i).split(",")
+                        p1 = Point(xyz[0], xyz[1], xyz[2])
+                    else:
+                        xyz = points(i).split(",")
+                        p2 = Point(xyz[0], xyz[1], xyz[2])
+                        edge = Line(p1, p2, color)
+                        edges.append(edge)
+                        p1 = None
+                        p2 = None
+            else:
+                if p1 is not None and (points(i) is not "ON" and points(i) is not "OFF"):
+                    xyz = points(i).split(",")
+                    p2 = Point(xyz[0], xyz[1], xyz[2])
+                    edge = Line(p1, p2, color)
+                    edges.append(edge)
+        return edges
+
